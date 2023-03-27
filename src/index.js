9953dd4e4103cc863ff6bdccea3712767fb53416
Bai Jaitrong <bjaitrong@comcast.net>
2023-03-27 17:02:53 -0600

    Refactor to add redux,react-redux, and add form-visible-reducer.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore} from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/ticket-list-reducer';
+
+const store = createStore(reducer);
+
+store.subscribe(() =>
+  console.log(store.getState())
+  );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
