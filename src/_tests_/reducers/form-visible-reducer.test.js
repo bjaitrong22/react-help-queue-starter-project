9fb682302f356c31199b92765f876024ba097189
Bai Jaitrong <bjaitrong@comcast.net>
2023-03-27 17:52:00 -0600

    Add root reducer.

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -8,5 +8,5 @@ describe("formVisibleReducer", () => {
 
   test('Should toggle form visibility state to true', () => {
     expect(formVisibleReducer(false, {type: 'TOGGLE_FORM'})).toEqual(true);
-  })
+  });
 });
