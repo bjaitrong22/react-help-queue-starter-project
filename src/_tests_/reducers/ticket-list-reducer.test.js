f689e6be2a2302d0333ac2acf493df53f1de2d3b
Bai Jaitrong <bjaitrong@comcast.net>
2023-04-03 19:22:31 -0600

    Refactor to add action constants.

--- src/_tests_/reducers/ticket-list-reducer.test.js
+++ src/_tests_/reducers/ticket-list-reducer.test.js
@@ -1,4 +1,6 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
+import * as c from '../../actions/ActionTypes';
+import { formatDistanceToNow } from 'date-fns';
 
 describe('ticketListReducer', () => {
 
@@ -17,10 +19,15 @@ describe('ticketListReducer', () => {
   }
 
   let action;
+
   const ticketData = {
     names: 'Ryan & Aimen',
     location: '4b',
     issue: 'Redux action is not working correctly.',
+    timeOpen: new Date(),
+    formattedWaitTime: formatDistanceToNow(new Date(), {
+      addSuffix: true
+    }),
     id: 1
   };
 
@@ -28,29 +35,51 @@ describe('ticketListReducer', () => {
     expect(ticketListReducer( {}, { type: null })).toEqual({});
   });
 
-  test('Should successfully add new ticket data to mainTicketList', () => {
-    const { names, location, issue, id } = ticketData;
+  test('should successfully add a ticket to the ticket list that includes date-fns-formatted wait times', () => {
+    const { names, location, issue, timeOpen, formattedWaitTime, id } = ticketData;
     action = {
-      type: 'ADD_TICKET',
-      names: names,
+      type: c.ADD_TICKET,
+      names: names, 
       location: location,
       issue: issue,
+      timeOpen: timeOpen,
+      formattedWaitTime: formattedWaitTime,
       id: id
-    }
-
+    };
     expect(ticketListReducer({}, action)).toEqual({
       [id]: {
         names: names,
         location: location,
-        issue: issue,
+        issue: issue, 
+        timeOpen: timeOpen,
+        formattedWaitTime: 'less than a minute ago',
         id: id
       }
     });
   });
+  // test('Should successfully add new ticket data to mainTicketList', () => {
+  //   const { names, location, issue, id } = ticketData;
+  //   action = {
+  //     type: c.ADD_TICKET,
+  //     names: names,
+  //     location: location,
+  //     issue: issue,
+  //     id: id
+  //   }
+
+  //   expect(ticketListReducer({}, action)).toEqual({
+  //     [id]: {
+  //       names: names,
+  //       location: location,
+  //       issue: issue,
+  //       id: id
+  //     }
+  //   });
+  // });
 
   test('Should successfully delete a ticket', () => {
     action = {
-      type:  'DELETE_TICKET',
+      type:  c.DELETE_TICKET,
       id: 1
     };
     expect(ticketListReducer(currentState,action)).toEqual({
@@ -59,6 +88,25 @@ describe('ticketListReducer', () => {
         locaton: '2a',
         issue: 'Reducer has side effects',
         id: 2
+      }
+    });
+  });
+
+  test('Should add a formatted wait time to ticket entry', () => {
+    const { names, location, issue, timeOpen, id } = ticketData;
+    action = {
+      type: c.UPDATE_TIME,
+      formattedWaitTime: '4 minutes ago',
+      id: id
+    };
+    expect(ticketListReducer({[id]: ticketData}, action)).toEqual({
+      [id]: {
+        names: names,
+        location: location, 
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: '4 minutes ago'
       }
     });
   });
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -17,6 +17,24 @@ class TicketControl extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(), 1000);
+  }
+
+  componentDidUpdate() {
+    console.log("component updated!")
+  }
+
+  componentWillUnmount() {
+    console.log("component unmounted!");
+    clearInterval(this.waitTimeUpdateTime);
+  }
+
+  updateTicketElapsedWaitTime = () => {
+    console.log("tick");
+  }
+  
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
