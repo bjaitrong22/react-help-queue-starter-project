e38750a61c26bbf0f6209ad04da1182effec17a7
Bai Jaitrong <bjaitrong@comcast.net>
2023-04-10 20:04:10 -0600

    Refactor code to read databse.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
 import EditTicketForm from './EditTicketForm';
 import TicketDetail from './TicketDetail';
 import db from './../firebase.js';
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, onSnapshot, deleteDoc } from "firebase/firestore";
+
 
 function TicketControl () {
   
@@ -12,14 +13,28 @@ function TicketControl () {
   const [mainTicketList, setMainTicketList] = useState([]);
   const [selectedTicket, setSelectedTicket] = useState(null);
   const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     selectedTicket: null,
-  //     editing: false
-  //   };
-  // }
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      collection(db, "tickets"),
+      (collectionSnapshot) => {
+        const tickets = [];
+        collectionSnapshot.forEach((doc) => {
+          tickets.push({
+            ... doc.data(),
+            id: doc.id
+          });
+        });
+        setMainTicketList(tickets);
+      },
+      (error) => {
+        setError(error.message);
+      }
+    );
+
+    return () => unSubscribe();
+  }, []);
 
   const handleClick = () => {
     if (selectedTicket != null) {
@@ -63,7 +78,9 @@ function TicketControl () {
     let currentlyVisibleState = null;
     let buttonText = null; 
 
-    if (editing ) {      
+    if (error) {
+      currentlyVisibleState = <p>There was an error: {error}</p>
+    } else if (editing ) {      
       currentlyVisibleState = 
       <EditTicketForm 
         ticket = {selectedTicket} 
@@ -92,7 +109,7 @@ function TicketControl () {
     return (
       <>
         {currentlyVisibleState}
-        <button onClick={handleClick}>{buttonText}</button> 
+        { error ? null: <button onClick={handleClick}>{buttonText}</button> }
       </>
     );
 }
